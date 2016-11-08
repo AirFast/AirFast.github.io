c188fd156993dfd615fb53ecac6794af6076c9e3
Andrew <AirFast.88@gmail.com>
2016-11-08 14:15:40 +0200

    Revert "adc"
    This reverts commit 3da28fe211b1eda3a18847640ae23dc71fd43264.

---
+++ Shiftwork.io/js/common.js
@@ -0,0 +1,75 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	$("#my-video").backgroundVideo({
+		$videoWrap: $("#video-wrap"),
+		$outerWrap: $(window),
+		preventContextMenu: true,
+		parallaxOptions: {
+			offset: 60,
+			effect: 1.7
+		}
+	});
+
+	/*function heightDetect() {
+		$("#video-wrap, #my-video").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});*/
+
+	$(".sandwich").click(function() {
+		$(".sandwich").toggleClass("active");
+		$('.menu').slideToggle(50);
+	});
+
+	$(window).resize(function() {
+		if($(window).width() > 768) {
+			$(".sandwich").removeClass("active");
+			$(".menu").removeAttr("style");
+		}
+	});
+
+	$(".logo, .menu ul li").click(function(){
+		$(".sandwich").removeClass("active");
+		$(".menu").removeAttr("style");
+	});
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
