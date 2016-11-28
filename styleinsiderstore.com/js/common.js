4da289ccfda8b1e4a1d9f93212ccaaefb4be7fea
Andrew <AirFast.88@gmail.com>
2016-11-28 15:08:47 +0200

    test

---
+++ styleinsiderstore.com/js/common.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	// Custom JS
+
+	$('#slider').owlCarousel({
+		items: 1,
+		loop: true,
+		nav: true,
+		navText: ['', ''],
+		autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true
+	})
+
+});
