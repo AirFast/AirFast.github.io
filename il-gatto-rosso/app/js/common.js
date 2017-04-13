7e0eed5d7e71a7eae25ec36d6d0b83039f1683e4
AirFast <airfast.88@gmail.com>
2017-04-13 12:22:23 +0300

    slider

--- il-gatto-rosso/app/js/common.js
+++ il-gatto-rosso/app/js/common.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	// Custom JS
+	// Slider Owl Carousel
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        autoplay: true,
+        autoplayTimeout: 7000,
+        autoplayHoverPause: true
+    });
 
 });
