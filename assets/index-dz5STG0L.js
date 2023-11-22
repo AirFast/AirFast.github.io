ee930968162ed4d78eab7497280df158452d3bbe
AirFast <airfast.88@gmail.com>
2023-11-22 20:57:20 +0200

    Updates

--- assets/index-dz5STG0L.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,b as r}from"./index-3JnJmO4Z.js";const t={};function c(a,n){return o(),r("h1",null,"Favorites")}const _=e(t,[["render",c]]);export{_ as default};
---
+++ assets/index-ttISt6sI.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-hjQSNTQy.js";const r={};function t(a,n){return c(),o("h1",null,"Favorites")}const _=e(r,[["render",t]]);export{_ as default};
